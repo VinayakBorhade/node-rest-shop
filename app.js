83c1d7c96965129ba148f4944df3fa022500b6f5
Vin_04 <vinayakborhade@gmail.com>
2019-09-21 15:18:19 +0530

    error handling included

--- app.js
+++ app.js
@@ -11,4 +11,19 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use(function(req, res, next){
+    const error=new Error('not found');
+    error.status=404;
+    next(error);
+});
+
+app.use(function(error, req, res, next){
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 module.exports=app;
