5e7c68d71aad8af3c22a20168e3a409da1d1280b
Vin_04 <vinayakborhade@gmail.com>
2019-09-21 15:08:37 +0530

    adding morgan for routes logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const app=express();
+const morgan=require('morgan');
 
 const productRoutes=require('./api/routes/products');
 const orderRoutes=require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // adding middleware for this rest api
 app.use('/products', productRoutes);
