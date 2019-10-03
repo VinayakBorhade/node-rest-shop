4f898ffc3b6b5aaa6d410663940c5181c68cc5c1
Vin_04 <vinayakborhade@gmail.com>
2019-10-03 19:58:04 +0530

    using multer for storing the uploaded files locally

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const router=express.Router();
 const mongoose=require('mongoose');
+const multer=require('multer');
+const upload=multer({dest: 'uploads/'});
 
 const Product=require('../models/product');
 
@@ -34,7 +36,8 @@ router.get('/', function(req, res, next){
     });
 });
 
-router.post('/', function(req, res, next){
+router.post('/', upload.single('productImage'), function(req, res, next){
+    console.log(req.file);
     const product=new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
