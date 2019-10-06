a286101926494dd6d397987e39940515aa074065
Vin_04 <vinayakborhade@gmail.com>
2019-10-06 10:38:58 +0530

    adding user login and jwt signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const mongoose=require('mongoose');
 const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 
 const User=require('../models/user');
 
@@ -45,6 +46,47 @@ router.post('/signup', function(req, res, next){
                 }
             });
         }
+    });
+});
+
+router.post('/login', function(req, res, next){
+    User
+    .find({email: req.body.email})
+    .exec()
+    .then(function(user){
+        if(user.length<1){
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, function(err, result){
+            if(err){
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            if(result){
+                const token=jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, process.env.JWT_KEY, {
+                    expiresIn: '1h'
+                });
+                return res.status(200).json({
+                    message: 'Auth successful',
+                    token: token
+                });
+            }
+            return res.status(401).json({
+                message: 'Auth failed'
+            });
+        });
+    })
+    .catch(function(err){
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
